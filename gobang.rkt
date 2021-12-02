e0eb9d346ae6cfbc43a256a064c9580b869816e9
ChengxiX <61776833+ChengxiX@users.noreply.github.com>
2021-12-02 17:40:27 +0800

    12/2

--- gobang.rkt
+++ gobang.rkt
@@ -1,10 +1,6 @@
 #lang racket
 (define node (lambda (depth aim situ alphabeta) (
-                                                 if (< depth aim) ;(
-                                                                   ;if (= (remainder depth 2) 0)
-                                                                      ;(maxnode depth aim situ 0 alphabeta -1e20 (let ((dia (generate-diagonol situ))) (generate-aval-map dia (car (car dia)) (car (cdr (car dia))) situ)))
-                                                                      ;(minnode depth aim situ 0 alphabeta 1e20 (let ((dia (generate-diagonol situ))) (generate-aval-map dia (car (car dia)) (car (cdr (car dia))) situ)))
-                                                                      ;)
+                                                 if (< depth aim)
                                                     (branch (if (= (remainder depth 2) 0) < >) depth aim situ 0 alphabeta -1e20 (let ((dia (generate-diagonol situ))) (generate-aval-map dia (car (car dia)) (car (cdr (car dia))) situ)))
                                                     (list (value situ) situ)
                                                     )))
@@ -32,6 +28,10 @@
 (define generate-aval-map (lambda (diagonol x y situ) (if (<= x (car (cdr diagonol))) (if (member (cons x y) situ) (if (= y (car (cdr (cdr diagonol)))) (generate-aval-map diagonol (+ 1 x) (car (cdr (car diagonol))) situ) (generate-aval-map diagonol x (+ 1 y) situ)) (cons (cons x y) (if (= y (car (cdr (cdr diagonol)))) (generate-aval-map diagonol (+ 1 x) (car (cdr (car diagonol))) situ) (generate-aval-map diagonol x (+ 1 y) situ)))) null)))
 
 (define put-xy-in-lines (lambda (situ depth) (if (eq? situ null) (list (hash) (hash) (hash) (hash)) (let* ((xy (car situ)) (next (put-xy-in-lines (cdr situ) (+ depth 1))) (vertical (first next)) (horizontal (second next)) (topleft-bottomright (third next)) (topright-bottomleft (fourth next)) (value-vertical (hash-ref vertical (car xy) #f)) (value-horizontal (hash-ref horizontal (car (cdr xy)) #f)) (value-topleft-bottomright (hash-ref topleft-bottomright (- (car (cdr xy)) (car xy)) #f)) (value-topright-bottomleft (hash-ref topright-bottomleft (+ (car (cdr xy)) (car xy)) #f)) (selves? (if (= (remainder depth 2) AI-first) #t #f)))
-;chez fist啥的得写出来
-                                                                                                      (;....~!
-                                                                                                       )))))
+;chez fist啥的得写出来,#t是ai下的,#f是人类（对手下的）
+                                                                                                      (list (if value-vertical (hash-set (hash-remove vertical (car xy)) (car xy) (cons (cons (car (cdr xy)) selves?) value-vertical)) (hash-set vertical (car xy) (list (cons (car (cdr xy)) selves?))))
+                                                                                                            (if value-horizontal (hash-set (hash-remove horizontal (car (cdr xy))) (car (cdr xy)) (cons (cons (car xy) selves?) value-horizontal)) (hash-set horizontal (car (cdr xy)) (list (cons (car xy) selves?))))
+                                                                                                            (if value-topleft-bottomright (hash-set (hash-remove topleft-bottomright (- (car (cdr xy)) (car xy))) (- (car (cdr xy)) (car xy)) (cons (cons (+ (car (cdr xy)) (car xy)) selves?) value-topleft-bottomright)) (hash-set topleft-bottomright (- (car (cdr xy)) (car xy)) (list (cons (+ (car (cdr xy)) (car xy)) selves?))))
+                                                                                                            (if value-topright-bottomleft (hash-set (hash-remove topright-bottomleft (+ (car (cdr xy)) (car xy))) (+ (car (cdr xy)) (car xy)) (cons (cons (+ (car (cdr xy)) (car xy)) selves?) value-topright-bottomleft)) (hash-set topright-bottomleft (+ (car (cdr xy)) (car xy)) (list (cons (- (car (cdr xy)) (car xy)) selves?))))
+                                                                                                            )
+                                                                                                       ))))
