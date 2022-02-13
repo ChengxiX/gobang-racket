bf5c5c9919672d2b83d7dddcb78c27a547359b8f
ChengxiX <hanson.xcx@outlook.com>
2022-02-13 18:35:46 +0800

    存档

--- gobang.rkt
+++ gobang.rkt
@@ -6,7 +6,7 @@
                                                     )))
 
 (define branch (lambda (opposite depth aim situ alpha beta able) (
-                                                          let ((x (node (+ depth 1) aim (cons (car able) situ) beta))) (if (and (opposite (car x) alpha) (not (eq? (cdr able) null)))
+                                                          let ((x (node (+ depth 1) aim (cons (car able) situ) beta))) (if (and (opposite (car x) alpha) (not (equal? (cdr able) null)))
                                                                                                                            (let ((y (branch opposite depth aim situ alpha (if (opposite beta (car x)) (car x) beta) (cdr able)))) (if (opposite (car y) (car x)) x y)) x
                                                                                                                            ))))
 
@@ -32,7 +32,7 @@
 
 (define generate-aval-map (lambda (diagonol x y situ) (if (<= x (car (car (cdr diagonol)))) (if (member (cons x y) situ) (if (= y (cdr (car (cdr diagonol)))) (generate-aval-map diagonol (+ 1 x) (cdr (car diagonol)) situ) (generate-aval-map diagonol x (+ 1 y) situ)) (cons (cons x y) (if (= y (cdr (car (cdr diagonol)))) (generate-aval-map diagonol (+ 1 x) (cdr (car diagonol)) situ) (generate-aval-map diagonol x (+ 1 y) situ)))) null)))
 
-(define put-xy-in-lines (lambda (situ depth) (if (eq? situ null) (list (hash) (hash) (hash) (hash)) (let* ((xy (car situ)) (next (put-xy-in-lines (cdr situ) (+ depth 1))) (vertical (first next)) (horizontal (second next)) (topleft-bottomright (third next)) (topright-bottomleft (fourth next)) (value-vertical (hash-ref vertical (car xy) #f)) (value-horizontal (hash-ref horizontal (cdr xy) #f)) (value-topleft-bottomright (hash-ref topleft-bottomright (- (cdr xy) (car xy)) #f)) (value-topright-bottomleft (hash-ref topright-bottomleft (+ (cdr xy) (car xy)) #f)) (selves? (= (remainder depth 2) AI-first)))
+(define put-xy-in-lines (lambda (situ depth) (if (equal? situ null) (list (hash) (hash) (hash) (hash)) (let* ((xy (car situ)) (next (put-xy-in-lines (cdr situ) (+ depth 1))) (vertical (first next)) (horizontal (second next)) (topleft-bottomright (third next)) (topright-bottomleft (fourth next)) (value-vertical (hash-ref vertical (car xy) #f)) (value-horizontal (hash-ref horizontal (cdr xy) #f)) (value-topleft-bottomright (hash-ref topleft-bottomright (- (cdr xy) (car xy)) #f)) (value-topright-bottomleft (hash-ref topright-bottomleft (+ (cdr xy) (car xy)) #f)) (selves? (= (remainder depth 2) AI-first)))
 ;chez fist啥的得写出来,#t是ai下的,#f是人类（对手下的）
                                                                                                       (list (if value-vertical (hash-set (hash-remove vertical (car xy)) (car xy) (cons (cons (cdr xy) selves?) value-vertical)) (hash-set vertical (car xy) (list (cons (cdr xy) selves?))))
                                                                                                             (if value-horizontal (hash-set (hash-remove horizontal (cdr xy)) (cdr xy) (cons (cons (car xy) selves?) value-horizontal)) (hash-set horizontal (cdr xy) (list (cons (car xy) selves?))))
@@ -44,10 +44,10 @@
 (define sortbycar (lambda (line) (sort line (lambda (x y) (< (car x) (car y))))))
 ;valueline递归返回(value 自己的坐标 落子方 连击次数)
 ;需要((1 #t) (3 #f))
-(define valueline (lambda (line) (if (eq? (cdr line) null) (list 0 (car (car line)) (cdr (car line)) 1) (let ((a (valueline (cdr line)))) 
+(define valueline (lambda (line) (if (equal? (cdr line) null) (list 0 (car (car line)) (cdr (car line)) 1) (let ((a (valueline (cdr line)))) 
                                                             (if (= (car (cdr (cdr (cdr a)))) 5) (list (+ (if (car (cdr (cdr a))) (* attack-ratio 成五) (* -1 成五)) (car a)) (car (car line)) (cdr (car line)) 0) (let ((diff (- (car (cdr a)) (car (car line))))) (if (= diff 1) (if (not (xor (cdr (car line)) (car (cdr (cdr a))))) (list (car a) (car (car line)) (cdr (car line)) (+ 1 (car (cdr (cdr (cdr a)))))) (list (+ (car a) (if (= (round (car a)) (car a)) (if (cdr (car line)) (* -1 (hash-ref valuetable (- (car (cdr (cdr (cdr a)))) 0.5) 0)) (* (hash-ref valuetable (- (car (cdr (cdr (cdr a)))) 0.5) 0) attack-ratio)) 0)) (car (car line)) (cdr (car line)) 0.5)) (if (= diff 2) (if (not (xor (cdr (car line)) (car (cdr (cdr a))))) (list (car a) (car (car line)) (cdr (car line)) (+ 0.5 (car (cdr (cdr (cdr a)))))) (list (+ (car a) (if (cdr (car line)) (hash-ref valuetable (car (cdr (cdr (cdr a)))) 0) (* (hash-ref valuetable (car (cdr (cdr (cdr a)))) 0) attack-ratio))) (car (car line)) (cdr (car line)) 1)) (list (+ (car a) (if (not (car (cdr (cdr a)))) (* -1 (hash-ref valuetable (car (cdr (cdr (cdr a)))) 0)) (* (hash-ref valuetable (car (cdr (cdr (cdr a)))) 0) attack-ratio))) (car (car line)) (cdr (car line)) 1))) ))
                                                             ))))
-(define value_lines_ (lambda (table keys) (if (eq? keys null) 0 (+ (car (let ((aim-line (sortbycar (hash-ref table (car keys))))) (valueline (cons (cons -100000 (not (cdr (car aim-line)))) aim-line)))) (value_lines_ table (cdr keys))))))
+(define value_lines_ (lambda (table keys) (if (equal? keys null) 0 (+ (car (let ((aim-line (sortbycar (hash-ref table (car keys))))) (valueline (cons (cons -100000 (not (cdr (car aim-line)))) aim-line)))) (value_lines_ table (cdr keys))))))
 (define null '())
 (define 成五 100000)
 (define 活四 100)
@@ -57,9 +57,9 @@
 (define 活二 5)
 (define 眠二 1)
 (define valuetable (hash '4.5 成五  '4 活四 '3.5 冲四 '3 活三 '2.5 眠三 '2 活二 '1.5 眠二))
-(define attack-ratio 0.3);进攻系数大于1进攻型，小于1防守型；建议先手时进攻，后手时防守
+(define attack-ratio 1);进攻系数大于1进攻型，小于1防守型；建议先手时进攻，后手时防守
 
-(define AI-first 1) ;如果是0则是人先手，如果是1则是AI先手，且AI先手时深度为偶数，人先手时为奇数
+(define AI-first 0) ;如果是0则是人先手，如果是1则是AI先手，且AI先手时深度为偶数，人先手时为奇数
 (define set-AI-first (lambda (a) (set! AI-first a)))
 ;testing
-;(node 0 2 (list (cons 4 6) (cons 4 5) (cons 2 4) (cons 3 4) (cons 3 5) (cons 4 4) (cons 9 5) (cons 6 2) (cons 4 3) (cons 6 3) (cons 8 2) (cons 7 3) (cons 7 4) (cons 6 4) (cons 5 2) (cons 5 3) (cons 5 7) (cons 5 5) (cons 9 7) (cons 5 4) (cons 6 5) (cons 5 6) (cons 7 6) (cons 8 9) (cons 8 7) (cons 8 4) (cons 8 6) (cons 6 8) (cons 8 8) (cons 7 7)) 1e20)
+(node 0 1 (list (cons 6 8) (cons 12 8) (cons 10 8) (cons 4 8) (cons 9 8) (cons 5 7) (cons 7 8) (cons 6 6) (cons 8 8)) 1e20)
